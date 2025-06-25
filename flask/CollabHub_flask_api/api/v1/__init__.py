dd3722ed5812a8551f09ec9ca3db5d531eee1348
chigbu <yungryce@yahoo.com>
2025-06-25 12:35:02 +0100

    Delete flask directory

--- flask/CollabHub_flask_api/api/v1/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Initialize Blueprint views"""
-from flask import Blueprint
-
-
-app_views = Blueprint("app_views", __name__, url_prefix="/api/v1/tasks")
-
-# Import and register Flask resources
-from api.v1.task_view import *
